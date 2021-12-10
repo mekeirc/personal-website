af2cd93f69d88e2658d97cfd4651a7ea5f8915cc
mekeirc <mekeirc@gmail.com>
2021-12-10 19:45:48 +0000

    Add generic portfolio square component

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -11,28 +11,33 @@ import HellfireSquare from '../images/portfolio-hellfiregfx.jpg';
 import RightindemSquare from '../images/portfolio-rightindem.png';
 import BuyingButlerSquare from '../images/portfolio-buyingbutler.png';
 
+const Square = ({ query, title, image }) => (
+    <div className="col-11 col-sm-6 col-lg-4">
+        <Link to={`/portfolio?source=${query}`} className="text-decoration-none">
+            <Heading as="h4" color="white" className="mt-3">{ title }</Heading>
+            <div className="d-flex bg-dark align-items-center justify-content-center">
+                <img src={ image } alt="image" className="img-fluid rounded w-100" />
+            </div>
+        </Link>
+    </div>
+);
+
 const Portfolio = () => (
     <div className="container-fluid" id="portfolio">
         <Section background="black" className="row">
             <MaxWidthContainer>
                 <Heading as="h2" color="white" className="text-center">Portfolio &amp; Demos</Heading>
                 <MobileView className="row">
-                    <div className="col-11 col-sm-6 col-lg-4">
-                        <Link to={`/portfolio?source=${queryStrings.HELLFIRE_GFX}`} className="text-decoration-none">
-                            <Heading as="h4" color="white" className="mt-3">HellfireGFX</Heading>
-                            <div className="d-flex bg-dark align-items-center justify-content-center">
-                                <img src={ HellfireSquare } alt="image" className="img-fluid rounded w-100" />
-                            </div>
-                        </Link>
-                    </div>
-                    <div className="col-11 col-sm-6 col-lg-4">
-                        <Link to={`/portfolio?source=${queryStrings.RIGHTINDEM}`} className="text-decoration-none">
-                            <Heading as="h4" color="white" className="mt-3">Rightindem</Heading>
-                            <div className="d-flex bg-dark align-items-center justify-content-center">
-                                <img src={ RightindemSquare } alt="image" className="img-fluid rounded w-100" />
-                            </div>
-                        </Link>
-                    </div>
+                    <Square
+                        query={ queryStrings.HELLFIRE_GFX }
+                        title="Hellfire GFX"
+                        image={ HellfireSquare }
+                    />
+                    <Square
+                        query={ queryStrings.RIGHTINDEM }
+                        title="Rightindem"
+                        image={ RightindemSquare }
+                    />
                 </MobileView>
             </MaxWidthContainer>
         </Section>
